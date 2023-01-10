a65bd16ea9368826fdb634c126935e2c09c13054
Pragades Rajagopal <66718413+Pragades-Rajagopal@users.noreply.github.com>
2023-01-10 18:02:04 +0000

    feature-1: Initial commit

--- crowdsourcing/workers/admin.py
+++ crowdsourcing/workers/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Audios
 
+admin.site.site_header = 'Crowdsourcing application'
+admin.site.site_title = 'App admin page'
+admin.site.index_title = 'Welcome, Admin!'
+
 
 class AudiosAdmin (admin.ModelAdmin):
     model = Audios
